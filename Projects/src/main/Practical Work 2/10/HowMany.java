0c0f1a767673938d3b6a742b59fc7f9929b6cce3
melch <melchakow.d@mail.ru>
2022-09-08 11:17:39 +0300

    Git init

--- Projects/src/main/Practical Work 2/10/HowMany.java
+++ Projects/src/main/Practical Work 2/10/HowMany.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class HowMany {
     public static void main(String[] args) {
+        Scanner s;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Кол-во слов: " + scanner.nextLine().split("\\s").length);
     }
